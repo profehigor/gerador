94d3cab3e8c5c97766be43be786e2660add16a68
profehigor <165037678+profehigor@users.noreply.github.com>
2024-08-29 11:21:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('generateButton').addEventListener('click', function() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('dogImage').src = data.message;
+        })
+        .catch(error => console.error('Erro ao buscar imagem:', error));
+});
