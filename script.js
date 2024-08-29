f406d770ef94338d410a8bc93449a11aff2becdd
profehigor <165037678+profehigor@users.noreply.github.com>
2024-08-29 11:29:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ document.getElementById('generateButton').addEventListener('click', function() {
         })
         .catch(error => console.error('Erro ao buscar imagem:', error));
 });
+
+const apiKey = 'Yn3pdNj8eZvZvAQVagjrtq8EGPd27YvcXPb6Bpni'; // Substitua com a chave de API fornecida pela NASA
+
+document.getElementById('generateButton').addEventListener('click', function() {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            const imgElement = document.getElementById('animalImage');
+            imgElement.src = data.url;
+            imgElement.alt = data.title; // Adiciona o título como texto alternativo
+            imgElement.style.display = 'block'; // Exibe a imagem
+        })
+        .catch(error => console.error('Erro ao buscar imagem:', error));
+});
